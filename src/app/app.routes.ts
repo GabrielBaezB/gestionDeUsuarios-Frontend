d15c506553cb5b6694d6567d5f932f149084ab64
GabrielBaezB <gabooo@live.com>
2024-10-28 16:51:33 -0300

    se agrega primeflex y primeng

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomeComponent,
+        title: 'Pagina de inicio'
+    },
+    {
+        path:'user-form/:id',
+        component:UserFormComponent,
+        title:'Formulario de usuarios'
+    },
+    {
+        path:'**',
+        redirectTo:'',
+        pathMatch:'full'
+    }
+
+];
